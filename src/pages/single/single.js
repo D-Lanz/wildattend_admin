8bc715c4467fcfdb4a1cb97bfa883ff1bcf2b83f
paez-marie1019 <paez.marie1019@gmail.com>
2024-04-17 01:10:58 +0800

    Single page frontend WIP
    1:58:09
    https://www.youtube.com/watch?v=yKV1IGahXqA

--- src/pages/single/single.js
+++ src/pages/single/single.js
@@ -1,9 +1,29 @@
 import "./single.css"
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Single = () => {
   return(
     <div className="single">
-        <h1>Single</h1>
+        <Sidebar/>
+        <div className="singleContainer">
+          <Navbar/>
+          <div className="left">
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://img.game8.co/3619988/99ba4ee9abf0e4a88d54da22b63455ee.png/show"
+                alt=""
+                className="itemImg"/>
+            </div>
+          </div>
+          <div className="right">
+
+          </div>
+          <div className="bottom">
+
+          </div>
+        </div>
     </div>
   )
 }
