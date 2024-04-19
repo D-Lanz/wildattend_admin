09acb4e91d69a4f7e6ea31124f83b6c9ea31a69f
paez-marie1019 <paez.marie1019@gmail.com>
2024-04-19 22:28:07 +0800

    Image Upload
    54:54
    https://www.youtube.com/watch?v=D9W7AFeJ3kk

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,3 +27,5 @@ export const auth = getAuth();
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
--- src/pages/new/new.js
+++ src/pages/new/new.js
@@ -1,25 +1,72 @@
 import "./new.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 import { collection, doc, setDoc, addDoc, serverTimestamp } from "firebase/firestore"; 
-import { auth, db } from "../../firebase"
+import { auth, db, storage } from "../../firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const New = ({inputs, title}) => {
-
   const [file,setFile] = useState("");
   const [data,setData] = useState({});
+  const [perc,setPerc] = useState(null);
+
+  useEffect(()=>{
+    const uploadFile = () => {
+      const name = new Date().getTime() + file.name
+      console.log(name)
+
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          setPerc(progress);
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+              default:
+                break;
+          }
+        }, 
+        (error) => {
+          // Handle unsuccessful uploads
+          console.log(error)
+        }, 
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setData((prev)=>({...prev, img:downloadURL}))
+          });
+        }
+      );
+    };
+    file && uploadFile();
+  },[file]);
 
   const handleInput = (e) => {
     const id = e.target.id;
     const value = e.target.value;
-
+ 
     setData({...data, [id]: value})
   }
 
-  console.log(data)
+  
 
   const handleAdd = async (e) => {
     e.preventDefault()
@@ -75,7 +122,7 @@ const New = ({inputs, title}) => {
                   onChange={handleInput}/>
               </div>
               ))}
-              <button className="buttonn" type="submit">Submit</button>
+              <button disabled={perc !== null && perc < 100} className="buttonn" type="submit">Submit</button>
             </form>
           </div>
         </div>
